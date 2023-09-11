ccd3af6dfba0e472532c982141f4d20d6ca6cdfc
Александр <34153613+sanybit@users.noreply.github.com>
2023-09-11 19:54:51 +0400

    Add files via upload

---
+++ views/page404.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<link href="<?php echo ROOT_HTML; ?>css/page404.css" rel="stylesheet" type="text/css">
+	<link rel="icon" href="views/favicon.ico">
+	<title>404</title>
+</head>
+<body>
+	<div class="section">
+    <h1 class="error">404</h1>
+    <div class="page"><?php echo $text; ?></div>
+    <a class="back-home" href="<?php echo ROOT_HTML.'task'; ?>">Вернуться домой</a>
+</div>
+</body>
+</html>

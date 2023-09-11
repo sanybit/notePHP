87b1e466f47fc20e1c43f578a0981a046aa58533
Александр <34153613+sanybit@users.noreply.github.com>
2023-09-11 20:02:43 +0400

    Update routes.php

--- app/config/routes.php
+++ app/config/routes.php
@@ -10,13 +10,6 @@ return array (
 	'note/registration/([0-9a-fA-F]{64})' => 'note/registration/$1',
 	'note/registration' => 'note/registration/$1',
 	'note' => 'note/index',
-	'task/edit/([0-9]+)' => 'task/view/$1',
-	'task/([0-9]+)' => 'task/index/$1',
-	'task/enter' => 'login/enter',
-	'task/add' => 'task/add',
-	'task' => 'task/index',
 	'(.*)' => '404', // любая строка
 );
-// 'task' - строка запроса
-// 'task/index' - имя контроллера и экшена для обработки этого запроса (путь обработчика)
 ?>
